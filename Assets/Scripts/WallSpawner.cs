785b2a8e7dc513891ad96ad2c4dbf39a2a7cb095
Eric Lathrop <eric@ericlathrop.com>
2017-06-10 13:04:58 -0400

    Make wall spawning

---
+++ Assets/Scripts/WallSpawner.cs
@@ -0,0 +1,58 @@
+﻿using Random = UnityEngine.Random;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WallSpawner : MonoBehaviour {
+
+  public GameObject[] walls;
+  public GameObject[] windows;
+
+  private float cameraWidth;
+  private float cameraHeight;
+  private float wallHeight;
+  private List<GameObject> items = new List<GameObject>();
+
+  // Use this for initialization
+  void Start() {
+    SetCameraDimensions();
+
+    var renderer = walls[0].GetComponent<SpriteRenderer>();
+    wallHeight = renderer.sprite.bounds.size.y;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    SetCameraDimensions();
+
+    var cameraTop = Camera.main.transform.position.y + (cameraHeight / 2f) + (wallHeight / 2f);
+    var top = cameraTop;
+
+    var bottomItem = items.LastOrDefault();
+    if (bottomItem) {
+      top = bottomItem.transform.position.y - wallHeight;
+    }
+
+    var bottom = Camera.main.transform.position.y - (cameraHeight / 2f) - (wallHeight / 2f);
+
+    for (var y = top; y > bottom; y -= wallHeight) {
+      var prefab = walls[Random.Range(0, walls.Length)];
+      GameObject instance = Instantiate(prefab, new Vector3(transform.position.x, y, 0f), Quaternion.identity);
+      items.Add(instance);
+    }
+
+    for (var i = items.Count - 1; i >= 0; i--) {
+      var item = items[i];
+      if (item.transform.position.y > cameraTop) {
+        items.RemoveAt(i);
+        Destroy(item);
+      }
+    }
+  }
+
+  private void SetCameraDimensions() {
+    Camera camera = Camera.main;
+    cameraHeight = 2f * camera.orthographicSize;
+    cameraWidth = cameraHeight * camera.aspect;
+  }
+}
