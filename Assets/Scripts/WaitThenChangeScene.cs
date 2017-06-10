68706656015312e97e1b66143658b96eb33ee719
Alex Bezuska <abezuska@gmail.com>
2017-06-10 12:07:28 -0400

    add two scoop scene

---
+++ Assets/Scripts/WaitThenChangeScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitThenChangeScene : MonoBehaviour {
+
+	public string sceneName;
+	public float secondsToWait;
+
+	void Start() {
+		Debug.Log ("I ran");
+		Invoke ("LoadScene", secondsToWait);
+	}
+
+	public void LoadScene() {
+		Debug.Log ("I am loadScene");
+		Application.LoadLevel(sceneName);
+	}
+}
