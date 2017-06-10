b0e8e056fcae9d804a3d2ec2d1048b13cc40c144
Eric Lathrop <eric@ericlathrop.com>
2017-06-09 22:31:00 -0400

    Add spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using Random = UnityEngine.Random;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+  public GameObject[] prefabs;
+
+  // Use this for initialization
+  void Start() {
+    var prefab = prefabs[Random.Range(0, prefabs.Length)];
+    GameObject instance = Instantiate(prefab, new Vector3(0, 0, 0f), Quaternion.identity);
+  }
+
+  // Update is called once per frame
+  void Update() {
+    
+  }
+}
