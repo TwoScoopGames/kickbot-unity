a17ab5ffb9610c41a27a28795b86fe8a5ef15a2b
Eric Lathrop <eric@ericlathrop.com>
2017-06-09 23:58:54 -0400

    Keep spawning bgs

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,6 +1,7 @@
 ﻿using Random = UnityEngine.Random;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour {
@@ -14,22 +15,27 @@ public class Spawner : MonoBehaviour {
   // Use this for initialization
   void Start() {
     SetCameraDimensions();
-
-    var prefab = prefabs[Random.Range(0, prefabs.Length)];
-    var renderer = prefab.GetComponent<SpriteRenderer>();
-
-    items = new List<GameObject>();
-    var top = Camera.main.transform.position.y - (cameraHeight / 2f);
-    var bottom = Camera.main.transform.position.y + (cameraHeight / 2f);
-    for (var y = top; y < bottom; y += renderer.sprite.bounds.size.y) {
-      GameObject instance = Instantiate(prefab, new Vector3(0, y, 0f), Quaternion.identity);
-      items.Add(instance);
-    }
   }
 
   // Update is called once per frame
   void Update() {
     SetCameraDimensions();
+
+    var prefab = prefabs[Random.Range(0, prefabs.Length)];
+    var renderer = prefab.GetComponent<SpriteRenderer>();
+
+    var top = Camera.main.transform.position.y + (cameraHeight / 2f);
+
+    var bottomItem = items.LastOrDefault();
+    if (bottomItem) {
+      top = bottomItem.transform.position.y - renderer.sprite.bounds.size.y;
+    }
+
+    var bottom = Camera.main.transform.position.y - (cameraHeight / 2f);
+    for (var y = top; y > bottom; y -= renderer.sprite.bounds.size.y) {
+      GameObject instance = Instantiate(prefab, new Vector3(0, y, 0f), Quaternion.identity);
+      items.Add(instance);
+    }
   }
 
   private void SetCameraDimensions() {
