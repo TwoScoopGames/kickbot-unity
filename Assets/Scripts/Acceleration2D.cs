cb69b38ee85f0839fbdf0798fe667734327fe545
Eric Lathrop <eric@ericlathrop.com>
2017-06-10 16:48:16 -0400

    Add gravity

---
+++ Assets/Scripts/Acceleration2D.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acceleration2D : MonoBehaviour {
+
+  public Vector3 acceleration;
+
+  private Movement2D movement;
+
+  // Use this for initialization
+  void Start () {
+    movement = GetComponent<Movement2D>();
+  }
+
+  // Update is called once per frame
+  void Update () {
+    movement.velocity += acceleration * Time.deltaTime;
+  }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+  private bool onWall = true;
+  private Movement2D movement;
+
+  // Use this for initialization
+  void Start () {
+    movement = GetComponent<Movement2D>();
+  }
+
+  // Update is called once per frame
+  void Update () {
+    var axis = Input.GetAxisRaw("Horizontal");
+    var left = axis < 0;
+    var right = axis > 0;
+
+    if (!onWall) {
+      return;
+    }
+
+    if (left) {
+      movement.velocity.x = -40;
+    } else if (right) {
+      movement.velocity.x = 40;
+    }
+
+    var wallIsOnLeft = transform.position.x < 0;
+
+    if (left) {
+      if (wallIsOnLeft) {
+        // sin wave bullshit
+      } else {
+        movement.velocity.x = -333.333f;
+      }
+      movement.velocity.y = 500;
+      onWall = false;
+      // jump sound
+    } else if (right) {
+      if (wallIsOnLeft) {
+        movement.velocity.x = 333.333f;
+      } else {
+        // sine wave bullshit
+      }
+      movement.velocity.y = 500;
+      onWall = false;
+      // jump sound
+    }
+  }
+}
