d5083840af4f71c877ae67a61e10efe1f6bc8822
Eric Lathrop <eric@ericlathrop.com>
2017-06-10 13:08:47 -0400

    Do cleanup on spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,33 +10,43 @@ public class Spawner : MonoBehaviour {
 
   private float cameraWidth;
   private float cameraHeight;
+  private float itemHeight;
   private List<GameObject> items = new List<GameObject>();
 
   // Use this for initialization
   void Start() {
     SetCameraDimensions();
+
+    var renderer = prefabs[0].GetComponent<SpriteRenderer>();
+    itemHeight = renderer.sprite.bounds.size.y;
   }
 
   // Update is called once per frame
   void Update() {
     SetCameraDimensions();
 
-    var prefab = prefabs[0];
-    var renderer = prefab.GetComponent<SpriteRenderer>();
-    var prefabHeight = renderer.sprite.bounds.size.y;
-
-    var top = Camera.main.transform.position.y + (cameraHeight / 2f);
+    var cameraTop = Camera.main.transform.position.y + (cameraHeight / 2f) + (itemHeight / 2f);
+    var top = cameraTop;
 
     var bottomItem = items.LastOrDefault();
     if (bottomItem) {
-      top = bottomItem.transform.position.y - prefabHeight;
+      top = bottomItem.transform.position.y - itemHeight;
     }
 
-    var bottom = Camera.main.transform.position.y - (cameraHeight / 2f) - prefabHeight;
-    for (var y = top; y > bottom; y -= prefabHeight) {
-      prefab = prefabs[Random.Range(0, prefabs.Length)];
+    var bottom = Camera.main.transform.position.y - (cameraHeight / 2f) - (itemHeight / 2f);
+
+    for (var y = top; y > bottom; y -= itemHeight) {
+      var prefab = prefabs[Random.Range(0, prefabs.Length)];
       GameObject instance = Instantiate(prefab, new Vector3(transform.position.x, y, 0f), Quaternion.identity);
       items.Add(instance);
+    }
+
+    for (var i = items.Count - 1; i >= 0; i--) {
+      var item = items[i];
+      if (item.transform.position.y > cameraTop) {
+        items.RemoveAt(i);
+        Destroy(item);
+      }
     }
   }
 
