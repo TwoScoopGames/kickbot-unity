51f9df25d76a811b3a7f742a0c11179293762d83
Eric Lathrop <eric@ericlathrop.com>
2017-06-09 23:06:58 -0400

    Tile background

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,14 +7,31 @@ public class Spawner : MonoBehaviour {
 
   public GameObject[] prefabs;
 
+  private float cameraWidth;
+  private float cameraHeight;
+
   // Use this for initialization
   void Start() {
+    SetCameraDimensions();
+
     var prefab = prefabs[Random.Range(0, prefabs.Length)];
-    GameObject instance = Instantiate(prefab, new Vector3(0, 0, 0f), Quaternion.identity);
+    var renderer = prefab.GetComponent<SpriteRenderer>();
+
+    var top = Camera.main.transform.position.y - (cameraHeight / 2f);
+    var bottom = Camera.main.transform.position.y + (cameraHeight / 2f);
+    for (var y = top; y < bottom; y += renderer.sprite.bounds.size.y) {
+      GameObject instance = Instantiate(prefab, new Vector3(0, y, 0f), Quaternion.identity);
+    }
   }
 
   // Update is called once per frame
   void Update() {
-    
+    SetCameraDimensions();
+  }
+
+  private void SetCameraDimensions() {
+    Camera camera = Camera.main;
+    cameraHeight = 2f * camera.orthographicSize;
+    cameraWidth = cameraHeight * camera.aspect;
   }
 }
