e8836db1c11428846cb2cc43929ed257c625a132
Eric Lathrop <eric@ericlathrop.com>
2017-06-11 11:23:21 -0400

    Add UI and animation

---
+++ Assets/Scripts/UIAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAnimation : MonoBehaviour {
+
+  public Sprite[] frames;
+  public float frameRate = 1f;
+
+  private int currentFrame;
+  private float time;
+  private Image renderer;
+
+  // Use this for initialization
+  void Start () {
+    renderer = GetComponent<Image>();
+  }
+
+  // Update is called once per frame
+  void Update () {
+    time += Time.deltaTime;
+
+    int framesToAdvance = (int)(time * frameRate);
+    currentFrame = (currentFrame + framesToAdvance) % frames.Length;
+    time -= framesToAdvance / frameRate;
+
+    renderer.sprite = frames[currentFrame];
+  }
+}
